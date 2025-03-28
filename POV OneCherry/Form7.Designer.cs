6f87b94779e96ac9c3fe3db79a1ab440164b1958
dxdavv <hwang.25lee@gmail.com>
2025-03-27 19:40:10 -0600

    nueva ventana registro de cliente

---
+++ POV OneCherry/Form7.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace POV_OneCherry
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Sitka Small", 18.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(68, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(240, 36);
+            label1.TabIndex = 0;
+            label1.Text = "Registrar cliente";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(70, 95);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(240, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Goudy Old Style", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(147, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 22);
+            label2.TabIndex = 2;
+            label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Goudy Old Style", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(147, 150);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 22);
+            label3.TabIndex = 3;
+            label3.Text = "Apellido:";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Goudy Old Style", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label4.Location = new Point(147, 305);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 22);
+            label4.TabIndex = 4;
+            label4.Text = "Email:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Goudy Old Style", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label5.Location = new Point(144, 227);
+            label5.Name = "label5";
+            label5.Size = new Size(74, 22);
+            label5.TabIndex = 5;
+            label5.Text = "Telefono:";
+            label5.Click += label5_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(53, 330);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(271, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(70, 252);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(240, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(70, 175);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(240, 23);
+            textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Crimson;
+            button1.Font = new Font("Goudy Old Style", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(143, 376);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 25);
+            button1.TabIndex = 9;
+            button1.Text = "Agregar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form7
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LavenderBlush;
+            ClientSize = new Size(384, 411);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form7";
+            Text = "Form7";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+    }
+}
